a80440261c0a1c55106b40cb1d97d820d6928586
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2022-01-11 11:59:01 +0530

    footer created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './Pages/header/header.component';
+import { LoginComponent } from './Pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Login', pathMatch: 'full' },
+
+  { path: 'Login', component: LoginComponent },
+  { path: 'Header', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
