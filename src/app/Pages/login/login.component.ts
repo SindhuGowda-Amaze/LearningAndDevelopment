64515f357ea0cf97eb123487824134e50b5bb53f
Rupali Pawar <naveen.amazeinc@gmail.com>
2022-01-11 15:25:45 +0530

    nnn

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -19,6 +19,36 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if (localStorage.getItem('temp') == '1') {
+      localStorage.clear();
+      location.reload();
+
+    }
+    this.GetLoginTypeMaster();
+  } 
+  GetLoginTypeMaster() {
+
+    
+  }
+
+  public getRoleID(even: any) {
+    debugger
+    this.roleID = even.target.value;
+  }
+
+  public login() {
+
+    if (this.userName == 'admin' && this.password == '1') {
+      debugger
+      sessionStorage.setItem('UserName', 'admin');
+      sessionStorage.setItem('temp', '1');
+      sessionStorage.setItem('role', 'Admin');
+      location.href="#/AdminDashboard"
+      sessionStorage.setItem('roleid', '1');
+      location.reload();
+
+      
+    }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { NgxDropzoneModule } from 'ngx-dropzone';
+import { NgxDropzoneModule } from 'ngx-dropzone';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Pages/login/login.component';
 import { HeaderComponent } from './Pages/header/header.component';
 import { FooterComponent } from './Pages/footer/footer.component';
-// import { AdminLoginComponent } from './Pages/admin-login/admin-login.component';
-// import { EmployeeLoginComponent } from './Pages/employee-login/employee-login.component';
-// import { ManagerLoginComponent } from './Pages/manager-login/manager-login.component';
 import { TrainerComponent } from './Pages/trainer/trainer.component';
 import { TrainerFormComponent } from './Pages/trainer-form/trainer-form.component';
 
@@ -18,16 +15,13 @@ import { TrainerFormComponent } from './Pages/trainer-form/trainer-form.componen
     LoginComponent,
     HeaderComponent,
     FooterComponent,
-    // AdminLoginComponent,
-    // EmployeeLoginComponent,
-    // ManagerLoginComponent
     TrainerComponent,
     TrainerFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // NgxDropzoneModule,
+    NgxDropzoneModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
