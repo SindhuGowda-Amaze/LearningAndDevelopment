6b78a071103fb8a346b03c6cabe128d6e991f446
Rupali Pawar <naveen.amazeinc@gmail.com>
2022-01-12 19:09:41 +0530

    nnn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,10 +17,10 @@ import { MyCourseDashboardComponent } from './Pages/my-course-dashboard/my-cours
 import { CatalogComponent } from './Pages/catalog/catalog.component';
 import { AdminDashboardComponent } from './Pages/admin-dashboard/admin-dashboard.component';
 import { ViewCourseComponent } from './Pages/view-course/view-course.component';
-import { StartMyCourseComponent } from './start-my-course/start-my-course.component';
 import { TakeAssessmentComponent } from './Pages/take-assessment/take-assessment.component';
 import { AssessmentResultComponent } from './Pages/assessment-result/assessment-result.component';
 import { CourseCertificateComponent } from './Pages/course-certificate/course-certificate.component';
+import { StartMyCourseComponent } from './Pages/start-my-course/start-my-course.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/Login', pathMatch: 'full' },
