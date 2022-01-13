a0bcef5492cae2c9dba9bc0a2c40588ae681cd75
Rupali Pawar <naveen.amazeinc@gmail.com>
2022-01-13 11:13:24 +0530

    NNN

--- src/app/Pages/catalog/catalog.component.ts
+++ src/app/Pages/catalog/catalog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2'
 
 
 @Component({
@@ -30,6 +31,28 @@ export class CatalogComponent implements OnInit {
   
 
   ngOnInit(): void {
+  }
+
+  enroll(){
+    Swal.fire({
+      title: 'Enroll Confirmation',
+      text: "Please click on OK to proceed with course enrolment.",
+      icon: 'warning',
+      // icon: 'success',
+      showCloseButton: true,
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'OK'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Enrolled',
+          'Successfully Assigned',
+          'success'
+        )
+      }
+    })
   }
 
 
