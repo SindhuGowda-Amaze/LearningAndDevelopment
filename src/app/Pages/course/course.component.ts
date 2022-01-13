2bd5fd2c5aa9f738641e4965dcd733bbc858c8d9
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2022-01-13 13:53:36 +0530

    chapter

--- src/app/Pages/course/course.component.ts
+++ src/app/Pages/course/course.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LearningService } from 'src/app/learning.service';
 
 @Component({
   selector: 'app-course',
@@ -8,29 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
  
-  constructor() { }
+  constructor(public LearningService:LearningService) { }
 
   ngOnInit(): void {
   }
   cancel() {
     location.href = "/CourseDashboard";
   }
+  Course_Photo:any;
+
+//   files: File[] = [];
+
+// onSelect(event:any) {
+//   console.log(event);
+//   this.files.push(...event.addedFiles);
+// }
+
+// onRemove(event:any) {
+//   console.log(event);
+//   this.files.splice(this.files.indexOf(event), 1);
+// }
 
 
-  files: File[] = [];
-
-onSelect(event:any) {
+files: File[] = [];
+onSelect(event: { addedFiles: any; }) {
+  debugger
   console.log(event);
-  this.files.push(...event.addedFiles);
+  this.files.push(event.addedFiles[0]);
+  this.uploadattachments();
+  console.log("content", this.files);
 }
 
-onRemove(event:any) {
-  console.log(event);
-  this.files.splice(this.files.indexOf(event), 1);
+
+onRemove(event:any)
+{
+debugger
+console.log(event);
+this.files.splice(this.files.indexOf(event),1);
 }
 
-
-
-
+public uploadattachments() {
+  debugger
+  this.LearningService.AttachmentsUpload(this.files).subscribe(res => {
+    debugger
+    this.Course_Photo = res;
+    alert("ATTACHMENT UPLOADED");
+  })
+}
 
 }
