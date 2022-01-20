00e7b459627c15fe74c20de2fc78b1400edc2e37
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2022-01-20 14:52:30 +0530

    sad

--- src/app/learning.service.ts
+++ src/app/learning.service.ts
@@ -414,11 +414,11 @@ public baseURL = "http://localhost:4199/";
       this.baseURL + "/Master/DeleteChapter?ID=" + ID);
   }
    
-  public InsertAssessments(json : any) {
-    debugger
-    let APIURL = this.baseURL + "Master/InsertAssessments";
-    return this.http.post<any[]>(APIURL,json);
-  }
+  // public InsertAssessments(json : any) {
+  //   debugger
+  //   let APIURL = this.baseURL + "Master/InsertAssessments";
+  //   return this.http.post<any[]>(APIURL,json);
+  // }
 
 
   public InsertAssessments(data: any) {
