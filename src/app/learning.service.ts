96fccbdb209908b9b39028d5ac7628d9b7ae9a07
Rupali Pawar <naveen.amazeinc@gmail.com>
2022-01-20 16:58:52 +0530

    N

--- src/app/learning.service.ts
+++ src/app/learning.service.ts
@@ -5,8 +5,8 @@ import {environment} from "../environments/environment";
   providedIn: 'root'
 })
 export class LearningService {
-public baseURL = "http://localhost:4199/";
-   //public baseURL = "http://103.133.214.197/LearningandDevelopment/";
+// public baseURL = "http://localhost:4199/";
+   public baseURL = "http://103.133.214.197/LearningandDevelopment/";
   public host = "https://digioffice.amazeone.co/digiofficeapi";
   
   url:any;
@@ -344,6 +344,11 @@ public baseURL = "http://localhost:4199/";
     return this.http.get<any[]>(APIURL);
   }
 
+  public InsertAssessments(json : any) {
+    debugger
+    let APIURL = this.baseURL + "Master/InsertAssessments";
+    return this.http.post<any[]>(APIURL,json);
+  }
 
   public UpdateAssessments(json : any) {
     debugger
@@ -415,11 +420,7 @@ public baseURL = "http://localhost:4199/";
       this.baseURL + "/Master/DeleteChapter?ID=" + ID);
   }
    
-  // public InsertAssessments(json : any) {
-  //   debugger
-  //   let APIURL = this.baseURL + "Master/InsertAssessments";
-  //   return this.http.post<any[]>(APIURL,json);
-  // }
+
 
   public InsertEnroll(data: any) {
     debugger;
