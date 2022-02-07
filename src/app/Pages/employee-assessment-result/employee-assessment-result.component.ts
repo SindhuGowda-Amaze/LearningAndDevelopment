a2558209166e512786e95e63fbb630dac7540335
Anusha <anushavalmiki.amaze@gmail.com>
2022-02-07 10:43:00 +0530

    commit

--- src/app/Pages/employee-assessment-result/employee-assessment-result.component.ts
+++ src/app/Pages/employee-assessment-result/employee-assessment-result.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LearningService } from 'src/app/learning.service';
 import Swal from 'sweetalert2';
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-employee-assessment-result',
@@ -85,6 +87,58 @@ export class EmployeeAssessmentResultComponent implements OnInit {
 
 
 
+  
+  download(){
+    this.convetToPDF1();
+  }
+
+
+
+
+
+
+
+
+  public convetToPDF1() {
+    debugger
+   
+    var data: any = document.getElementById('downloadaplication');
+    html2canvas(data).then(canvas => {  
+      var margin = 5;
+      var imgWidth = 208
+      // var pageHeight = 295 - 10 * margin;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+      var doc = new jsPDF('p', 'mm');
+      var position = 0;
+      while (heightLeft > 0) {
+
+        const contentDataURL = canvas.toDataURL('image/png')
+        position = heightLeft - imgHeight;
+
+        doc.addPage();
+        doc.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);     
+        heightLeft -= pageHeight;
+      }
+      doc.deletePage(1)
+      doc.save('ER-2 Report.pdf');
+      
+      var pdf1 = doc.output('blob');
+      var file = new File([pdf1], "Application.pdf");
+      let body = new FormData();
+      debugger
+      body.append('Dan', file);
+      console.log('pdf', pdf1)
+    }).then(() => {
+     
+    });;
+  }
+
+
+
+
+
   // public AllocateCertificate() {
   //   debugger
   //   var entity = {
@@ -101,6 +155,8 @@ export class EmployeeAssessmentResultComponent implements OnInit {
   //     })
 
   // }
+
+
 
 
 
